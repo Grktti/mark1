6ab69984656081ed447e5f9a71115649d2d8c42c
Rikuokato <21eh019@ms.dendai.ac.jp>
2024-10-01 19:47:01 +0900

    mapクラスにfield_maxを追加 座標から行列の成分に変換する関数作成

--- crlAgentMap.hpp
+++ crlAgentMap.hpp
@@ -19,6 +19,8 @@
 #include "include/crlGLFW.hpp"
 #include "crlAgentCore.hpp"
 #include "crlAgentGLFW.hpp"
+#define FIELD_MAX 100.0 // フィールドの大きさ
+
 
 
 namespace ac = agentcore;
@@ -71,6 +73,18 @@ public:
         }
         m_init_flg = true;
         return true;
+    }
+
+    bool trail_map(double x, double y){
+        std::vector<std::vector<int>> mark;
+        mark.resize(m_map_size[0]);
+        for (int i = 0; i < m_map_size[0]; i++) {
+            mark[i].resize(m_map_size[1], 0);
+        }
+        int i = (int)(-y+FIELD_MAX);
+        int j = (int)(x+FIELD_MAX);
+        mark[i][j]=1;
+
     }
 
     [[nodiscard]] bool is_arleady_exist(const std::vector<double>& pos) const {
--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 #include "crlAgentGLFW.hpp"
 #include "crljoystick.hpp"
 #include <thread>
-//#include "crlAgentmap.hpp"
+#include "crlAgentMap.hpp"
 
 crlAgentGLFW g_wnd; // GLFW ウィンドウ用クラス
 #define SAMPLING_TIME 0.033 // サンプリング時間 [sec]
@@ -46,37 +46,40 @@ void main_loop(int speedx) {
             // 一番近くのエージェント ID を取得 (int nearest_agent_id に代入)
             nearest_agent_id = agent[i].get_nearest_agent_id(agent);
 
-//            if (i < 5) {
-//                // エージェントのランダムウォーク入力を獲得 (u[0] = -5〜5, u[1] = -5〜5)
-//                u = agent[i].get_random_walk(5.0);
-//                // エージェントの駆動(入力は u[0], u[1])
-//                agent[i].drive(u, agent, SAMPLING_TIME);
-//                // 描画用にエージェントをセット [編集不要]
-//                g_wnd.set_obj(i, agent[i].get_pos(), _blue(), agent[i].get_radius(), false);
-//            } else if (i < 8) {
-//                // エージェントの入力
-//                u[0] = sin(sec);
-//                u[1] = cos(sec);
-//                // エージェントの駆動(入力は u[0], u[1])
-//                agent[i].drive(u, agent, SAMPLING_TIME);
-//                // 描画用にエージェントをセット [編集不要]
-//                g_wnd.set_obj(i, agent[i].get_pos(), _red(), agent[i].get_radius(), true);
-//            } else {
-//                // nearest_agent_id 方向へのベクトルを取得 u に代入
-//                u = agent[i].get_vect(agent[nearest_agent_id]);
-//                // u を正規化 （大きさを1に）
-//                normalize(u);
-//                // エージェントの駆動(入力は u[0], u[1])
-//                agent[i].drive(u, agent, SAMPLING_TIME);
-//                // 描画用にエージェントをセット [編集不要]
-//                g_wnd.set_obj(i, agent[i].get_pos(), _green(), agent[i].get_radius(), true);
-//            }
-            //エージェントのboidモデルによる駆動
-            u=agent[i].get_void_model(agent, AGENT_SIGHT);
-            agent[i].drive(u, agent, SAMPLING_TIME);
-            g_wnd.set_obj(i, agent[i].get_pos(), _green(), agent[i].get_radius(), true);
+            if (i < 5) {
+                // エージェントのランダムウォーク入力を獲得 (u[0] = -5〜5, u[1] = -5〜5)
+                u = agent[i].get_random_walk(5.0);
+                // エージェントの駆動(入力は u[0], u[1])
+                agent[i].drive(u, agent, SAMPLING_TIME);
+                // 描画用にエージェントをセット [編集不要]
+                g_wnd.set_obj(i, agent[i].get_pos(), _blue(), agent[i].get_radius(), false);
+            } else if (i < 8) {
+                // エージェントの入力
+                u[0] = sin(sec);
+                u[1] = cos(sec);
+                // エージェントの駆動(入力は u[0], u[1])
+                agent[i].drive(u, agent, SAMPLING_TIME);
+                // 描画用にエージェントをセット [編集不要]
+                g_wnd.set_obj(i, agent[i].get_pos(), _red(), agent[i].get_radius(), true);
+            } else {
+                // nearest_agent_id 方向へのベクトルを取得 u に代入
+                u = agent[i].get_vect(agent[nearest_agent_id]);
+                // u を正規化 （大きさを1に）
+                normalize(u);
+                // エージェントの駆動(入力は u[0], u[1])
+                agent[i].drive(u, agent, SAMPLING_TIME);
+                // 描画用にエージェントをセット [編集不要]
+                g_wnd.set_obj(i, agent[i].get_pos(), _green(), agent[i].get_radius(), true);
+            }
+            trail_map();
 
-
+//            //エージェントのboidモデルによる駆動
+//            u=agent[i].get_void_model(agent, AGENT_SIGHT);
+//            agent[i].drive(u, agent, SAMPLING_TIME);
+//            g_wnd.set_obj(i, agent[i].get_pos(), _green(), agent[i].get_radius(), true);
+//
+            //agentのナンバーと座標を表示
+            std::cout << "agent[" << i << "]: (" << agent[i].get_pos()[0] << ", " << agent[i].get_pos()[1] << ")" << std::endl;
         }
         // sleep [描画のために必要] 数値計算のみでは不要
         std::this_thread::sleep_for(
