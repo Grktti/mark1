bf941b009ea68fe44eba5cdeda85cff1730993bb
r.kato <21eh019@ms.dendai.ac.jp>
2024-12-25 19:10:46 +0900

    display関数にマーカーが表示されるようにプログラムした。（初期化ができてなくてエラーが出る）

--- main.cpp
+++ main.cpp
@@ -43,6 +43,8 @@ void main_loop(int speedx) {
     // メインループ ここを主に編集
     while (true) {
 
+        g_wnd.set_agent_map(&g_map);
+
 
         for (int i = 0; i < agent_num; i++) {
             // 一番近くのエージェント ID を取得 (int nearest_agent_id に代入)
@@ -52,7 +54,7 @@ void main_loop(int speedx) {
             u = agent[i].get_boid_model(agent, g_map);
             agent[i].drive(u, agent, SAMPLING_TIME);
             g_wnd.set_obj(i, agent[i].get_pos(), _green(), agent[i].get_radius(), true);
-            g_wnd.set_agent_map(&g_map);
+
 
             // エージェントの現在地をコンソールに出力
 //            agent[i].print_position(i);
